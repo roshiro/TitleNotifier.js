2f28041ac8d281ca64ed549c4a2d73be6fdfba69
Rafael Oshiro <rafael.oshiro@gmail.com>
2014-04-20 23:13:42 +0100

    Initial commit

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+(function() {
+  var addButton = document.getElementById('add_btn'),
+      subButton = document.getElementById('sub_btn'),
+      resButton = document.getElementById('res_btn');
+
+  addButton.onclick = function() {
+    titlenotifier.add();
+  };
+
+  subButton.onclick = function() {
+    titlenotifier.sub();
+  };
+
+  resButton.onclick = function() {
+    titlenotifier.reset();
+  };
+})();
---
+++ javascripts/title_notifier.js
@@ -0,0 +1,101 @@
+/* ===========================================================
+ * title_notifier.js
+ * ===========================================================
+ * Copyright 2014 Rafael Oshiro.
+ * http://www.frontendjournal.com
+ *
+ * Lightweight, dependency-free javascript library to dinamically
+ * show the number of unread notifications in your webpage title.
+ *
+ * https://github.com/roshiro/TitleNotifier.js
+ * ========================================================== */
+
+(function() {
+  var title = document.getElementsByTagName('title')[0],
+      notificationTotal = 0,
+      patt = /\(\d*\) /;
+
+  function updateTitle() {
+    if(notificationTotal === 0) {
+      title.text = title.text.replace(patt, "");
+      return;
+    }
+
+    if(patt.exec(title.text)) {
+      title.text = title.text.replace(patt, "("+ notificationTotal +") ");
+    } else {
+      title.text = "(" + notificationTotal + ") " + title.text;
+    }
+  };
+
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+
+  /**
+   * TitleNotifier Namespace.
+   */
+  window.titlenotifier = {
+
+    /**
+     * Adds 1 or {value} to total.
+     *
+     * @param {value} Number to be addeed to total. (Optional)
+     */
+    add: function(value) {
+      if(typeof value === "undefined") {
+        value = 1;
+      }
+
+      notificationTotal += parseInt(value,10);
+
+      updateTitle();
+    },
+
+    /**
+     * Subtracts 1 or {value} from total.
+     *
+     * @param {value} Number to be subtracted from total. (Optional)
+     */
+    sub: function(value) {
+      if(typeof value === "undefined") {
+        value = 1;
+      }
+      value = parseInt(value,10);
+
+      if(notificationTotal === 0) {
+        return;
+      } else if(value > notificationTotal) {
+        notificationTotal = 0;
+      } else {
+        notificationTotal -= parseInt(value,10);
+      }
+
+      updateTitle();
+    },
+
+    /**
+     * Sets {value} to total.
+     *
+     * @param {value} Number to be set as total.
+     */
+    set: function(value) {
+      if(!isNumber(value) || value < 0) {
+        return;
+      }
+
+      notificationTotal = parseInt(value, 10);
+      updateTitle();
+    },
+
+    /**
+     * Resets total to zero and resets title to original version.
+     */
+    reset: function() {
+      notificationTotal = 0;
+      updateTitle();
+    }
+
+  }
+
+})();
